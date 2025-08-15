f5c119d2ee227338c3bcd6eccc455553d18bd364
Artem Sorokin <artem@sorokin.pp.ru>
2025-08-15 14:36:59 +0300

    Add support for Home Assistant 2025.8+ (#1)

--- custom_components/device_tracker_template/device_tracker.py
+++ custom_components/device_tracker_template/device_tracker.py
@@ -1,10 +1,9 @@
 from typing import Any
 
 from homeassistant.components.device_tracker.const import ATTR_SOURCE_TYPE, SourceType
-from homeassistant.components.template.const import CONF_ATTRIBUTES, CONF_PICTURE
 from homeassistant.components.template.template_entity import TemplateEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_ICON, CONF_LOCATION, CONF_UNIQUE_ID, STATE_HOME, STATE_NOT_HOME
+from homeassistant.const import CONF_LOCATION, CONF_UNIQUE_ID, STATE_HOME, STATE_NOT_HOME
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.template import Template
@@ -25,13 +24,10 @@ class DeviceTrackerTemplateEntity(TemplateEntity):
         super().__init__(
             hass,
             config=config,
-            icon_template=config.get(CONF_ICON),
-            entity_picture_template=config.get(CONF_PICTURE),
-            attribute_templates=config.get(CONF_ATTRIBUTES),
             unique_id=config.get(CONF_UNIQUE_ID),
         )
 
-        self._template: Template = config[CONF_LOCATION]
+        self._template: Template | None = config[CONF_LOCATION]
         self._source_type_template: Template | None = config.get(CONF_SOURCE_TYPE)
         self._attr_extra_state_attributes[ATTR_SOURCE_TYPE] = SourceType.GPS
 
@@ -39,6 +35,7 @@ class DeviceTrackerTemplateEntity(TemplateEntity):
     def _async_setup_templates(self) -> None:
         super()._async_setup_templates()
 
+        assert self._template is not None
         self.add_template_attribute(
             "_attr_state", self._template, None, self._update_state, none_on_template_error=True
         )
